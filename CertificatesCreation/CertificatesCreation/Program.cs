804448d418817962ab063fdcb7aeb970b720165b
damienbod <damien_bod@hotmail.com>
2020-01-14 23:16:57 +0100

    adding a third cert

--- CertificatesCreation/CertificatesCreation/Program.cs
+++ CertificatesCreation/CertificatesCreation/Program.cs
@@ -1,10 +1,9 @@
-﻿using CertificateManager.Models;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
-
 using CertificateManager;
+
 namespace CertificatesCreation
 {
     class Program
@@ -43,21 +42,38 @@ namespace CertificatesCreation
 
             Console.WriteLine($"Created Intermediate Certificate {intermediateCertificate.SubjectName}");
 
+            var intermediateCertificateLevel3 = icCreator.CreateIntermediateCertificate(
+                IntermediateLevel3CertConfig.DistinguishedName,
+                IntermediateLevel3CertConfig.BasicConstraints,
+                IntermediateLevel3CertConfig.ValidityPeriod,
+                IntermediateLevel3CertConfig.SubjectAlternativeName,
+                intermediateCertificate);
+
+            intermediateCertificateLevel3.FriendlyName = "intermediate level 3 from intermediate";
+
+
             string password = "1234";
             string rootCertName = "localhost_root";
             string intermediateCertName = "localhost_intermediate";
+            string intermediateCertNameL3 = "localhost_intermediate_lthree";
 
             var importExportCertificate = serviceProvider.GetService<ImportExportCertificate>();
 
-            importExportCertificate.SaveCertificateToPfxFile($"{rootCertName}.pfx", password, rootCert, null, null);
+            importExportCertificate.SaveCertificateToPfxFile($"{rootCertName}.pfx", 
+                password, rootCert, null, null);
             var rootPublicKey = importExportCertificate.ExportCertificatePublicKey(rootCert);
             var rootPublicKeyBytes = rootPublicKey.Export(X509ContentType.Cert);
             File.WriteAllBytes($"{rootCertName}.cer", rootPublicKeyBytes);
 
             var chain = new X509Certificate2Collection();
-            var previousCaCertPublicKey = importExportCertificate.ExportCertificatePublicKey(rootCert);
-            importExportCertificate.SaveCertificateToPfxFile($"{intermediateCertName}.pfx", password, intermediateCertificate, previousCaCertPublicKey, chain);
-            chain.Add(previousCaCertPublicKey);
+            var previousCaCertPublicKeyRoot =  importExportCertificate.ExportCertificatePublicKey(rootCert);
+            importExportCertificate.SaveCertificateToPfxFile($"{intermediateCertName}.pfx", 
+                password, intermediateCertificate, previousCaCertPublicKeyRoot, chain);
+            
+            chain.Add(previousCaCertPublicKeyRoot);
+            var previousCaCertPublicKeyIntermediate = importExportCertificate.ExportCertificatePublicKey(intermediateCertificate);
+            importExportCertificate.SaveCertificateToPfxFile($"{intermediateCertNameL3}.pfx", 
+                password, intermediateCertificateLevel3, previousCaCertPublicKeyIntermediate, chain);
 
             Console.WriteLine($"Exported Certificates");
 
