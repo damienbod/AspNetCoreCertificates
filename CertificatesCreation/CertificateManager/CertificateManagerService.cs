f8bef9e534b3aadac23774d0c1ab82e0a381daf5
damienbod <damien_bod@hotmail.com>
2020-01-15 20:16:04 +0100

    Adding a new api

--- CertificatesCreation/CertificateManager/CertificateManagerService.cs
+++ CertificatesCreation/CertificateManager/CertificateManagerService.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CertificateManager.Models;
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace CertificateManager
@@ -20,5 +23,48 @@ namespace CertificateManager
             _deviceCertificate = deviceCertificate;
         }
 
+        /// <summary>
+        /// Create a root certificate for Client and Server TLS Auth
+        /// </summary>
+        /// <param name="distinguishedName">Distinguished Name used for the subject and the issuer properties</param>
+        /// <param name="validityPeriod">Valid from, Valid to certificate properties</param>
+        /// <param name="pathLengthConstraint">path length for the amount of chained certificates</param>
+        /// <param name="dnsName">Dns name use the certificate validation</param>
+        /// <returns>X509Certificate2 root self signed certificate</returns>
+        public X509Certificate2 CreateRootCertificateForClientServerAuth(
+            DistinguishedName distinguishedName,
+            ValidityPeriod validityPeriod,
+            int pathLengthConstraint,
+            string dnsName)
+        {
+            var enhancedKeyUsages = new OidCollection {
+                new Oid("1.3.6.1.5.5.7.3.2"), // TLS Client auth
+                new Oid("1.3.6.1.5.5.7.3.1")  // TLS Server auth
+            };
+
+            var basicConstraints = new BasicConstraints
+            {
+                CertificateAuthority = true,
+                HasPathLengthConstraint = true,
+                PathLengthConstraint = pathLengthConstraint,
+                Critical = true
+            };
+
+            var subjectAlternativeName = new SubjectAlternativeName
+            {
+                DnsName = new List<string>
+                {
+                    dnsName,
+                }
+            };
+            var rootCert = _rootCertificate.CreateRootCertificate(
+                distinguishedName,
+                basicConstraints,
+                validityPeriod,
+                subjectAlternativeName,
+                enhancedKeyUsages);
+
+            return rootCert;
+        }
     }
 }
---
+++ CertificatesCreation/CertificatesCreation/LowLevel/LowLevelApis.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using CertificateManager;
+using System.Security.Cryptography;
+
+namespace CertificatesCreation
+{
+    public static class LowLevelApis
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Create Root Certificate");
+
+            var serviceProvider = new ServiceCollection()
+                .AddCertificateManager()
+                .BuildServiceProvider();
+
+            var enhancedKeyUsages = new OidCollection {
+                new Oid("1.3.6.1.5.5.7.3.2"), // TLS Client auth
+                new Oid("1.3.6.1.5.5.7.3.1")  // TLS Server auth
+            };
+
+            var rcCreator = serviceProvider.GetService<RootCertificate>();
+
+            var rootCert = rcCreator.CreateRootCertificate(
+                RootCertConfig.DistinguishedName,
+                RootCertConfig.BasicConstraints,
+                RootCertConfig.ValidityPeriod,
+                RootCertConfig.SubjectAlternativeName,
+                enhancedKeyUsages);
+
+            rootCert.FriendlyName = "localhost root l1";
+
+            Console.WriteLine($"Created Root Certificate {rootCert.SubjectName}");
+
+
+            var icCreator = serviceProvider.GetService<IntermediateCertificate>();
+
+            var intermediateCertificate = icCreator.CreateIntermediateCertificate(
+                IntermediateCertConfig.DistinguishedName,
+                IntermediateCertConfig.BasicConstraints,
+                IntermediateCertConfig.ValidityPeriod,
+                IntermediateCertConfig.SubjectAlternativeName,
+                rootCert,
+                enhancedKeyUsages);
+
+            intermediateCertificate.FriendlyName = "intermediate from root l2";
+
+            Console.WriteLine($"Created Intermediate Certificate {intermediateCertificate.SubjectName}");
+
+            var intermediateCertificateLevel3 = icCreator.CreateIntermediateCertificate(
+                IntermediateLevel3CertConfig.DistinguishedName,
+                IntermediateLevel3CertConfig.BasicConstraints,
+                IntermediateLevel3CertConfig.ValidityPeriod,
+                IntermediateLevel3CertConfig.SubjectAlternativeName,
+                intermediateCertificate,
+                enhancedKeyUsages);
+
+            intermediateCertificateLevel3.FriendlyName = "intermediate l3 from intermediate";
+
+            var deviceCertCreator = serviceProvider.GetService<DeviceCertificate>();
+
+            var deviceCertificate = deviceCertCreator.CreateDeviceCertificate(
+                DeviceCertConfig.DistinguishedName,
+                DeviceCertConfig.BasicConstraints,
+                DeviceCertConfig.ValidityPeriod,
+                DeviceCertConfig.SubjectAlternativeName,
+                intermediateCertificateLevel3,
+                enhancedKeyUsages);
+
+            deviceCertificate.FriendlyName = "device cert l4";
+            
+
+            string password = "1234";
+      
+            var importExportCertificate = serviceProvider.GetService<ImportExportCertificate>();
+
+            importExportCertificate.SaveCertificateToPfxFile(
+                "localhost_root_l1.pfx", password, rootCert, null, null);
+            var rootPublicKey = importExportCertificate.ExportCertificatePublicKey(rootCert);
+            var rootPublicKeyBytes = rootPublicKey.Export(X509ContentType.Cert);
+            File.WriteAllBytes($"localhost_root_l1.cer", rootPublicKeyBytes);
+
+            var chain = new X509Certificate2Collection();
+            var previousCaCertPublicKeyRoot =  importExportCertificate.ExportCertificatePublicKey(rootCert);
+            importExportCertificate.SaveCertificateToPfxFile(
+                "localhost_intermediate_l2.pfx", password, intermediateCertificate, previousCaCertPublicKeyRoot, chain);
+            
+            chain.Add(previousCaCertPublicKeyRoot);
+            var previousCaCertPublicKeyIntermediate = importExportCertificate.ExportCertificatePublicKey(intermediateCertificate);
+            importExportCertificate.SaveCertificateToPfxFile("localhost_intermediate_l3.pfx", 
+                password, intermediateCertificateLevel3, previousCaCertPublicKeyIntermediate, chain);
+
+            //var (certificate, collection) = 
+            //   importExportCertificate.LoadCertificateAndCollectionFromPfx(SigningCertPfxFile, password);
+
+            importExportCertificate.SaveCertificateToPfxFile(
+                $"devicel4.pfx", password, deviceCertificate, intermediateCertificateLevel3, chain);
+
+            Console.WriteLine($"Exported Certificates");
+
+        }
+    }
+}
