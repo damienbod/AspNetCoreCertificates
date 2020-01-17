b8db873d1df3ef286a477015a7684c51814a4667
damienbod <damien_bod@hotmail.com>
2020-01-17 12:20:38 +0100

    improving method docs

--- src/CertificateManager/CertificateManagerService.cs
+++ src/CertificateManager/CertificateManagerService.cs
@@ -233,14 +233,24 @@ namespace CertificateManager
         /// <param name="distinguishedName">Distinguished Name used for the subject and the issuer properties</param>
         /// <param name="validityPeriod">Valid from, Valid to certificate properties</param>
         /// <param name="subjectAlternativeName">SAN but only DnsNames can be added as a list + Email property</param>
-        /// <param name="enhancedKeyUsages">Defines how the certificate key can be used. If this value is not defined, the key can be used for any purpose.
+        /// <param name="enhancedKeyUsages">Defines how the certificate key can be used. 
         ///  new Oid("1.3.6.1.5.5.7.3.1")  // TLS Server auth
         ///  new Oid("1.3.6.1.5.5.7.3.2")  // TLS Client auth
         ///  new Oid("1.3.6.1.5.5.7.3.3")  // Code signing 
         ///  new Oid("1.3.6.1.5.5.7.3.4")  // Email
         ///  new Oid("1.3.6.1.5.5.7.3.8")  // Timestamping  
         ///  </param>
-        /// <param name="x509KeyUsageFlags"></param>
+        /// <param name="x509KeyUsageFlags">Defines how the certificate key can be used. 
+        ///  None             No key usage parameters.
+        ///  EncipherOnly     The key can be used for encryption only.
+        ///  CrlSign          The key can be used to sign a certificate revocation list (CRL).
+        ///  KeyCertSign      The key can be used to sign certificates.
+        ///  KeyAgreement     The key can be used to determine key agreement, such as a key created using the Diffie-Hellman key agreement algorithm.
+        ///  DataEncipherment The key can be used for data encryption.
+        ///  KeyEncipherment  The key can be used for key encryption.
+        ///  NonRepudiation   The key can be used for authentication.
+        ///  DecipherOnly     The key can be used for decryption only.
+        ///  </param>
         /// <returns>Self signed certificate</returns>
         public X509Certificate2 CreateSelfSignedCertificate(
             DistinguishedName distinguishedName,
