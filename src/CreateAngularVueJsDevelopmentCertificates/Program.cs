cbfae8ae55b33111aa2b245eb878ee3f50a6b7b8
damienbod <damien_bod@hotmail.com>
2020-01-24 10:05:07 +0100

    Adding a development certificate

---
+++ src/CreateAngularVueJsDevelopmentCertificates/Program.cs
@@ -0,0 +1,28 @@
+﻿using CertificateManager;
+using CertificateManager.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CreateAngularVueJsDevelopmentCertificates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddCertificateManager()
+                .BuildServiceProvider();
+
+            var _createCertificatesRsa = serviceProvider.GetService<CreateCertificatesRsa>();
+
+            // Create development certificate for localhost
+            var rootCert = _createCertificatesRsa
+                .CreateDevelopmentCertificate("localhost", 10);
+
+            rootCert.FriendlyName = "localhost development";
+        }
+    }
+}
