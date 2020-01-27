3aa36d1195d2d76dc505c4ae4367f57b8f0064da
damienbod <damien_bod@hotmail.com>
2020-01-27 22:26:22 +0100

    adding chained test RSA certs

--- src/CertificateManagerTests/RsaKeySizeTests.cs
+++ src/CertificateManagerTests/RsaKeySizeTests.cs
@@ -13,6 +13,23 @@ namespace CertificateManagerTests
     public class RsaKeySizeTests
     {
         [Fact]
+        public void CreateChainedCertificatesRsaKeySizeTest()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddCertificateManager()
+                .BuildServiceProvider();
+
+            var cc = serviceProvider.GetService<CreateCertificates>();
+            var cert2048 = CreateRsaCertificate(cc, 2048);
+            var cert4096 = CreateRsaCertificate(cc, 4096);
+
+            var chained1024 = CreateRsaCertificateChained(cc, 1024, cert2048);
+            var chained4096 = CreateRsaCertificateChained(cc, 4096, cert2048);
+            Assert.Equal(1024, chained1024.PrivateKey.KeySize);
+            Assert.Equal(4096, chained4096.PrivateKey.KeySize);
+        }
+
+        [Fact]
         public void CreateCertificatesRsaKeySizeTest()
         {
             var serviceProvider = new ServiceCollection()
@@ -47,6 +64,61 @@ namespace CertificateManagerTests
         {
             var basicConstraints = new BasicConstraints
             {
+                CertificateAuthority = true,
+                HasPathLengthConstraint = true,
+                PathLengthConstraint = 2,
+                Critical = false
+            };
+
+            var subjectAlternativeName = new SubjectAlternativeName
+            {
+                DnsName = new List<string>
+                {
+                    "localhost",
+                }
+            };
+
+            var x509KeyUsageFlags = X509KeyUsageFlags.KeyCertSign
+               | X509KeyUsageFlags.DigitalSignature
+               | X509KeyUsageFlags.KeyEncipherment
+               | X509KeyUsageFlags.CrlSign
+               | X509KeyUsageFlags.DataEncipherment
+               | X509KeyUsageFlags.NonRepudiation
+               | X509KeyUsageFlags.KeyAgreement;
+
+            // only if mtls is used
+            var enhancedKeyUsages = new OidCollection
+            {
+                new Oid("1.3.6.1.5.5.7.3.1"),  // TLS Server auth
+                new Oid("1.3.6.1.5.5.7.3.2"),  // TLS Client auth
+                //new Oid("1.3.6.1.5.5.7.3.3"),  // Code signing 
+                //new Oid("1.3.6.1.5.5.7.3.4"),  // Email
+                //new Oid("1.3.6.1.5.5.7.3.8")   // Timestamping  
+            };
+
+            var certificate = createCertificates.NewRsaSelfSignedCertificate(
+                new DistinguishedName { CommonName = "localhost" },
+                basicConstraints,
+                new ValidityPeriod
+                {
+                    ValidFrom = DateTimeOffset.UtcNow,
+                    ValidTo = DateTimeOffset.UtcNow.AddYears(1)
+                },
+                subjectAlternativeName,
+                enhancedKeyUsages,
+                x509KeyUsageFlags,
+                new RsaConfiguration
+                {
+                    KeySize = keySize
+                });
+
+            return certificate;
+        }
+
+        public static X509Certificate2 CreateRsaCertificateChained(CreateCertificates createCertificates, int keySize, X509Certificate2 parentCert)
+        {
+            var basicConstraints = new BasicConstraints
+            {
                 CertificateAuthority = false,
                 HasPathLengthConstraint = false,
                 PathLengthConstraint = 0,
@@ -73,7 +145,7 @@ namespace CertificateManagerTests
                 //new Oid("1.3.6.1.5.5.7.3.8")   // Timestamping  
             };
 
-            var certificate = createCertificates.NewRsaSelfSignedCertificate(
+            var certificate = createCertificates.NewRsaChainedCertificate(
                 new DistinguishedName { CommonName = "localhost" },
                 basicConstraints,
                 new ValidityPeriod
@@ -82,6 +154,7 @@ namespace CertificateManagerTests
                     ValidTo = DateTimeOffset.UtcNow.AddYears(1)
                 },
                 subjectAlternativeName,
+                parentCert,
                 enhancedKeyUsages,
                 x509KeyUsageFlags,
                 new RsaConfiguration
