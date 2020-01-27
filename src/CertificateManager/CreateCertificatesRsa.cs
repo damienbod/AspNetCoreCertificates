d5280586ef33860de7659fac694d006bb45b082d
damienbod <damien_bod@hotmail.com>
2020-01-27 19:00:27 +0100

    adding some docs

--- src/CertificateManager/CreateCertificatesRsa.cs
+++ src/CertificateManager/CreateCertificatesRsa.cs
@@ -15,6 +15,13 @@ namespace CertificateManager
             _createCertificates = createCertificates;
         }
 
+        /// <summary>
+        /// creates a development certificate
+        /// </summary>
+        /// <param name="dnsName">DNS name ie localhost etc</param>
+        /// <param name="validityPeriodInYears">valid time in years</param>
+        /// <param name="keySize">1024 2048 4096</param>
+        /// <returns></returns>
         public X509Certificate2 CreateDevelopmentCertificate(string dnsName, int validityPeriodInYears, int keySize = 1024)
         {
             var basicConstraints = new BasicConstraints
