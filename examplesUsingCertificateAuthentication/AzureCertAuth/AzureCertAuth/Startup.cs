bd1a129f2670e5712b61fc0be60c4e7bacf52bef
damienbod <damien_bod@hotmail.com>
2022-08-20 17:35:29 +0200

    clean up

--- examplesUsingCertificateAuthentication/AzureCertAuth/AzureCertAuth/Startup.cs
+++ examplesUsingCertificateAuthentication/AzureCertAuth/AzureCertAuth/Startup.cs
@@ -1,112 +1,103 @@
-using System;
 using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Certificate;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace AzureCertAuth
+namespace AzureCertAuth;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<MyCertificateValidationService>();
+
+        services.AddCertificateForwarding(options =>
         {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton<MyCertificateValidationService>();
-
-            services.AddCertificateForwarding(options =>
+            options.CertificateHeader = "X-ARR-ClientCert";
+            options.HeaderConverter = (headerValue) =>
             {
-                options.CertificateHeader = "X-ARR-ClientCert";
-                options.HeaderConverter = (headerValue) =>
+
+                X509Certificate2? clientCertificate = null;
+                if (!string.IsNullOrWhiteSpace(headerValue))
                 {
+                    byte[] bytes = Convert.FromBase64String(headerValue);
+                    clientCertificate = new X509Certificate2(bytes);
+                }
 
-                    X509Certificate2 clientCertificate = null;
-                    if (!string.IsNullOrWhiteSpace(headerValue))
+                return clientCertificate;
+            };
+        });
+
+        services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
+            .AddCertificate(options => // code from ASP.NET Core sample
+            {
+                // https://docs.microsoft.com/en-us/aspnet/core/security/authentication/certauth
+                options.AllowedCertificateTypes = CertificateTypes.SelfSigned;
+                
+                // Default values
+                //options.AllowedCertificateTypes = CertificateTypes.Chained;
+                //options.RevocationFlag = X509RevocationFlag.ExcludeRoot;
+                //options.RevocationMode = X509RevocationMode.Online;
+                //options.ValidateCertificateUse = true;
+                //options.ValidateValidityPeriod = true;
+
+                options.Events = new CertificateAuthenticationEvents
+                {
+                    OnCertificateValidated = context =>
                     {
-                        byte[] bytes = Convert.FromBase64String(headerValue);
-                        clientCertificate = new X509Certificate2(bytes);
-                    }
+                        var validationService =
+                            context.HttpContext.RequestServices.GetService<MyCertificateValidationService>();
 
-                    return clientCertificate;
+                        if (validationService!.ValidateCertificate(context.ClientCertificate))
+                        {
+                            var claims = new[]
+                            {
+                                new Claim(ClaimTypes.NameIdentifier, context.ClientCertificate.Subject, ClaimValueTypes.String, context.Options.ClaimsIssuer),
+                                new Claim(ClaimTypes.Name, context.ClientCertificate.Subject, ClaimValueTypes.String, context.Options.ClaimsIssuer)
+                            };
+
+                            context.Principal = new ClaimsPrincipal(new ClaimsIdentity(claims, context.Scheme.Name));
+                            context.Success();
+                        }
+                        else
+                        {
+                            context.Fail("invalid cert");
+                        }
+
+                        return Task.CompletedTask;
+                    }
                 };
             });
 
-            services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
-                .AddCertificate(options => // code from ASP.NET Core sample
-                {
-                    // https://docs.microsoft.com/en-us/aspnet/core/security/authentication/certauth
-                    options.AllowedCertificateTypes = CertificateTypes.SelfSigned;
-                    
-                    // Default values
-                    //options.AllowedCertificateTypes = CertificateTypes.Chained;
-                    //options.RevocationFlag = X509RevocationFlag.ExcludeRoot;
-                    //options.RevocationMode = X509RevocationMode.Online;
-                    //options.ValidateCertificateUse = true;
-                    //options.ValidateValidityPeriod = true;
+        services.AddAuthorization();
+        services.AddControllers();
+    }
 
-                    options.Events = new CertificateAuthenticationEvents
-                    {
-                        OnCertificateValidated = context =>
-                        {
-                            var validationService =
-                                context.HttpContext.RequestServices.GetService<MyCertificateValidationService>();
-
-                            if (validationService.ValidateCertificate(context.ClientCertificate))
-                            {
-                                var claims = new[]
-                                {
-                                    new Claim(ClaimTypes.NameIdentifier, context.ClientCertificate.Subject, ClaimValueTypes.String, context.Options.ClaimsIssuer),
-                                    new Claim(ClaimTypes.Name, context.ClientCertificate.Subject, ClaimValueTypes.String, context.Options.ClaimsIssuer)
-                                };
-
-                                context.Principal = new ClaimsPrincipal(new ClaimsIdentity(claims, context.Scheme.Name));
-                                context.Success();
-                            }
-                            else
-                            {
-                                context.Fail("invalid cert");
-                            }
-
-                            return Task.CompletedTask;
-                        }
-                    };
-                });
-
-            services.AddAuthorization();
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            //app.UseCertificateForwarding();
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        //app.UseCertificateForwarding();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
     }
 }
--- examplesUsingCertificateAuthentication/AzureCertAuth/AzureCertAuthClientConsole/Program.cs
+++ examplesUsingCertificateAuthentication/AzureCertAuth/AzureCertAuthClientConsole/Program.cs
@@ -1,44 +1,41 @@
-﻿using System;
-using System.Net.Http;
-using System.Security.Cryptography.X509Certificates;
+﻿using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
-using System.Threading.Tasks;
 
-namespace AzureCertAuthClientConsole
+namespace AzureCertAuthClientConsole;
+
+class Program
 {
-    class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        Console.WriteLine("Get data!");
+
+        var json = await GetApiDataUsingHttpClientHandler();
+
+        Console.WriteLine("Success!");
+        Console.ReadLine();
+    }
+
+    private static async Task<JsonDocument> GetApiDataUsingHttpClientHandler()
+    {
+        var cert = new X509Certificate2("client.pfx", "1234");
+        var handler = new HttpClientHandler();
+        handler.ClientCertificates.Add(cert);
+        var client = new HttpClient(handler);
+
+        var request = new HttpRequestMessage()
         {
-            Console.WriteLine("Get data!");
-
-            var json = await GetApiDataUsingHttpClientHandler();
-
-            Console.WriteLine("Success!");
+            RequestUri = new Uri("https://azurecertauth20201108214641.azurewebsites.net/WeatherForecast"),
+            Method = HttpMethod.Get,
+        };
+        var response = await client.SendAsync(request);
+        if (response.IsSuccessStatusCode)
+        {
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseContent);
+            var data = JsonDocument.Parse(responseContent);
+            return data;
         }
 
-        private static async Task<JsonDocument> GetApiDataUsingHttpClientHandler()
-        {
-            var cert = new X509Certificate2("client.pfx", "1234");
-            var handler = new HttpClientHandler();
-            handler.ClientCertificates.Add(cert);
-            var client = new HttpClient(handler);
-
-            var request = new HttpRequestMessage()
-            {
-                RequestUri = new Uri("https://azurecertauth20201108214641.azurewebsites.net/WeatherForecast"),
-                Method = HttpMethod.Get,
-            };
-            var response = await client.SendAsync(request);
-            if (response.IsSuccessStatusCode)
-            {
-                var responseContent = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(responseContent);
-                var data = JsonDocument.Parse(responseContent);
-                return data;
-            }
-
-            throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
-        }
+        throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
     }
 }
