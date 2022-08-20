a4890ce9f5cfcb9e1b54f0113981defeb53dc8c3
damienbod <damien_bod@hotmail.com>
2022-08-20 16:59:35 +0200

    Update examples

--- examplesUsingCertificateAuthentication/AspNetCoreChained/AspNetCoreCertificateAuthApiChained/Startup.cs
+++ examplesUsingCertificateAuthentication/AspNetCoreChained/AspNetCoreCertificateAuthApiChained/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-
 namespace AspNetCoreCertificateAuthApi
 {
     public class Startup
